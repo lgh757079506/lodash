1cc5f4c54403489191c6b3fbff2ca52593474d2d
李光辉 <ligh02@mingyuanyun.com>
2019-09-25 19:16:43 +0800

    完成compact函数

--- index.js
+++ index.js
@@ -35,3 +35,23 @@ _.chunk = function(array = [], size = 1) {
 
     return result
 }
+/**
+ * array (Array): 需要处理的数组
+ * 创建一个新数组，包含原数组中所有的非假值元素。例如false, null, 0, "", undefined, 和 NaN 都是被认为是“假值”。
+ * **/
+_.compact = function(array = []) {
+    let result = []
+
+    if (array == null) {
+        return result
+    }
+
+    for(let i = 0, len = array.length; i < len; i++) {
+        const currentItem = array[i]
+        
+        if (currentItem) {
+            result.push(currentItem)
+        }
+    }
+    return result
+}
